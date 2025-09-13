8ce5e2cdde4a3a2bb54b2a05a3cdaa589e52f1af
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-09-13 23:51:28 +0530

    credentails updated

--- backend/utils/googleSheets.js
+++ backend/utils/googleSheets.js
@@ -1,5 +1,3 @@
-// utils/googleSheets.js
-import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { google } from "googleapis";
@@ -8,10 +6,20 @@ import { google } from "googleapis";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// === Load credentials ===
-const credentials = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "../role-credentials.json"), "utf-8")
-);
+// === Load credentials from environment variables ===
+const credentials = {
+  type: process.env.GCP_TYPE,
+  project_id: process.env.GCP_PROJECT_ID,
+  private_key_id: process.env.GCP_PRIVATE_KEY_ID,
+  private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GCP_CLIENT_EMAIL,
+  client_id: process.env.GCP_CLIENT_ID,
+  auth_uri: process.env.GCP_AUTH_URI,
+  token_uri: process.env.GCP_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GCP_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GCP_CLIENT_CERT_URL,
+  universe_domain: process.env.GCP_UNIVERSE_DOMAIN,
+};
 
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 
